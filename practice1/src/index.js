5571206cd6478f7701fcfa2bf987b740a213142a
jose <jose>
2019-07-17 16:44:01 -0500

    did more tutorials

--- practice1/src/index.js
+++ practice1/src/index.js
@@ -1,12 +1,98 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// class HelloWorld extends React.Component {
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             username: 'josemeono'
+//         }
+//         this.handleChange = this.handleChange.bind(this)
+//     }
+//     handleChange(e){
+//         this.setState({
+//             username: e.target.value
+//         })
+//     }
+//     render(){
+//         return <div>Hello {this.state.username}! <br></br>
+//         Change Name:
+//         <input type="text" value={this.state.username} onChange= {this.handleChange}>
+//         </input>
+//         </div>
+//     }
+// }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Parent Component
+class FriendsContainer extends React.Component {
+    constructor(props){
+        super(props)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        this.state = {
+            name : 'Jose Meono',
+            friends : ['John', 'Sherry', 'Bailey', 'Barb', 'Justin'],
+        }
+        this.addFriend = this.addFriend.bind(this)
+    }
+    addFriend(friend){
+        this.setState((state)=> ({
+            friends: state.friends.concat([friend])
+        }))
+    }
+    render(){
+        return(
+            <div>
+                <h3>Name: {this.state.name} </h3>
+                <AddFriend addNew={this.addFriend}></AddFriend>
+                <ShowList names={this.state.friends}></ShowList>
+            </div>
+        )
+    }
+}
+
+//Child Component
+class ShowList extends React.Component{
+    render(){
+        return(
+            <div>
+                <h3>Friends</h3>
+                <ul>
+                    {this.props.names.map((friend) => {return <li>{friend}</li>})}
+                </ul>
+            </div>
+        )
+    }
+}
+
+class AddFriend extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            newFriends : ''
+        }
+
+        this.updateNewFriend = this.updateNewFriend.bind(this)
+        this.handleAddNew = this.handleAddNew.bind(this)
+
+    }
+    updateNewFriend(e){
+        this.setState({
+            newFriend : e.target.value
+        })
+    }
+    handleAddNew(){
+        this.props.addNew(this.state.newFriend)
+        this.setState({
+            newFriend: ''
+        })
+    }
+    render(){
+        return (
+            <div>
+                <input type="text" value={this.state.newFriend} onChange={this.updateNewFriend}></input>
+                <button onClick={this.handleAddNew}> Add Friend</button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<FriendsContainer />, document.getElementById('root'))
