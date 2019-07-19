991f358b7f32ce7d795860fe331939b49e3ef772
jose <jose>
2019-07-19 16:19:41 -0500

    trying to center calculator with css

--- calculator-test/calculator/src/index.js
+++ calculator-test/calculator/src/index.js
@@ -1,3 +1,6 @@
+/* eslint no-eval: 0 */
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -7,22 +10,40 @@ class App extends React.Component{
         super(props)
 
         this.state = {
-            number1 : 0,
-            number2 : 0,
-            result: 0
+            number1 : '',
+            result: '',
+            count: props.count
         }
-        this.updateValues = this.updateValues.bind(this)
+        // this.updateValues = this.updateValues.bind(this)
     }
-    updateValues(e){
-        // we get the e.target.name (which will be either "number1" or "number2") and use it 
-        // to target the key on our 'state' object with the same name using bracket syntax
-        this.setState({
-            [e.target.name] : e.target.value 
-        })
+    // updateValues(e){
+    //     // we get the e.target.name (which will be either "number1" or "number2") and use it 
+    //     // to target the key on our 'state' object with the same name using bracket syntax
+    //     this.setState({
+    //         [e.target.name] : e.target.value 
+    //     })
+    // }
+
+    onClick = button => {
+        console.log(button)
+        if(button === "="){
+            this.calculateValues()
+        }
+        else if(button === "CLEAR"){
+            this.setState({
+                result: '',
+            })
+        }
+        else{
+            this.setState({
+                result : this.state.result + button
+            })
+        }
+        
     }
-    calculateValues(number1, number2){
+    calculateValues(){
         this.setState({
-            result: (1 * number1) + (1 * number2),
+            result: eval(this.state.result),
         })
     }
     
@@ -31,15 +52,11 @@ class App extends React.Component{
 
         return(
             <div>
-                <h1>Calculator App</h1>
-                <label>Test: </label>
-                <input type="text" name="number1"  onChange={this.updateValues}></input>
-                <label>Test2:</label>
-                <input type="text" name="number2"  onChange={this.updateValues}></input>
+                <h1>Calculator App:</h1>
                 <div>
-                    <button onClick={() => this.calculateValues(this.state.number1,this.state.number2)}>Add</button>
-                    <h3>Result: {this.state.result}</h3>
-                    <Calculator number1={this.state.number1}></Calculator>
+                    <Result result={this.state.result}></Result>
+                    <Calculator onClick={this.onClick}></Calculator>
+                    
                 </div>
             </div>
         )
@@ -49,16 +66,48 @@ class App extends React.Component{
 class Calculator extends React.Component{
     render() {
         return(
-            <div className="Calculator">
-                {this.props.number1}
+            <div className="Calculator" >
+                <button name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
+                <button name="2" onClick={e => this.props.onClick(e.target.name)}>2</button>
+                <button name="3" onClick={e => this.props.onClick(e.target.name)}>3</button>
+                <br></br>
+                <button name="4" onClick={e => this.props.onClick(e.target.name)}>4</button>
+                <button name="5" onClick={e => this.props.onClick(e.target.name)}>5</button>
+                <button name="6" onClick={e => this.props.onClick(e.target.name)}>6</button>
+                <br></br>
+                <button name="7" onClick={e => this.props.onClick(e.target.name)}>7</button>
+                <button name="8" onClick={e => this.props.onClick(e.target.name)}>8</button>
+                <button name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
+                <br></br>
+                <button name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
+                <button name="+" onClick={e => this.props.onClick(e.target.name)}>+</button>
+                <button name="-" onClick={e => this.props.onClick(e.target.name)}>-</button>
+                <br></br>
+                <button name="*" onClick={e => this.props.onClick(e.target.name)}>*</button>
+                <button name="/" onClick={e => this.props.onClick(e.target.name)}>/</button>
+                <button name="=" onClick={e => this.props.onClick(e.target.name)}>=</button>
+                <br></br>
+                <button name="CLEAR" onClick={e => this.props.onClick(e.target.name)}>CLEAR</button>
+
             </div>
         )
     }
 }
 
-class Form extends React.Component{
-
+class Result extends React.Component{
+    render() {
+        let {result} = this.props
+        return(
+            <div className="result">
+                <p>{result}</p>
+            </div>
+        )
+    }
 }
+
+App.defaultProps = {
+    count: 1
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
